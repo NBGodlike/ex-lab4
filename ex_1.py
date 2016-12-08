b7a5ab53158c9f033750be5f262cf250fc585a28
NBGodlike <artem.markov.96@mail.ru>
2016-12-09 00:07:15 +0300

    Update ex_1.py

--- ex_1.py
+++ ex_1.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from librip.gens import field
+from librip.gens import gen_random
 
 goods = [
     {'title': 'Ковер', 'price': 2000, 'color': 'green'},
@@ -8,4 +9,9 @@ goods = [
     {'title': 'Вешалка для одежды', 'price': 800, 'color': 'white'}
 ]
 
+
 # Реализация задания 1
+
+print(list(field(goods,'title')))
+print(list(field(goods,'title','price')))
+print(list(gen_random(1,3,5)))
